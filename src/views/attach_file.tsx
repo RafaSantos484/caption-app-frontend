c64af9346a263e7f2202c47d6513e765522435ca
rafael.santos <rafaelsantos32141@gmail.com>
2024-11-16 14:54:06 -0300

    Inserção de select 'Idioma'

--- src/views/attach_file.tsx
+++ src/views/attach_file.tsx
@@ -2,7 +2,11 @@ import { HighlightOff } from "@mui/icons-material";
 import {
   Button,
   createTheme,
+  FormControl,
   IconButton,
+  InputLabel,
+  MenuItem,
+  Select,
   ThemeProvider,
   Tooltip,
 } from "@mui/material";
@@ -10,11 +14,15 @@ import { useRef, useState } from "react";
 
 import "./attach_file.scss";
 import axios from "axios";
+import { Language, languagesDict } from "../types";
 
 const themes = createTheme({
   palette: {
     primary: {
       main: "rgb(255, 61, 61)",
+    },
+    secondary: {
+      main: "rgb(255, 255, 255)",
     },
   },
 });
@@ -29,6 +37,7 @@ export default function AttachFile() {
   const [videoWithSubsInfo, setVideoWithSubsInfo] = useState<
     VideoInfo | null | undefined
   >(undefined);
+  const [language, setLanguage] = useState<Language | "auto">("auto");
 
   const hiddenFileInput = useRef<HTMLInputElement>(null);
 
@@ -72,74 +81,109 @@ export default function AttachFile() {
         </div>
 
         <div className="attach-container">
-          {(() => {
-            if (!!videoWithSubsInfo) {
-              return (
-                <Button
-                  variant="contained"
-                  onClick={() => {
-                    if (
-                      window.confirm(
-                        "O vídeo atual com legenda será descartado. Continuar?"
-                      )
-                    ) {
-                      setVideoInfo(undefined);
-                      setVideoWithSubsInfo(undefined);
+          <div className="options-container">
+            {(() => {
+              if (!!videoWithSubsInfo) {
+                return (
+                  <Button
+                    variant="contained"
+                    onClick={() => {
+                      if (
+                        window.confirm(
+                          "O vídeo atual com legenda será descartado. Continuar?"
+                        )
+                      ) {
+                        setVideoInfo(undefined);
+                        setVideoWithSubsInfo(undefined);
+                        hiddenFileInput.current?.click();
+                      }
+                    }}
+                  >
+                    Usar Novamente
+                  </Button>
+                );
+              } else if (!videoInfo) {
+                return (
+                  <Button
+                    variant="contained"
+                    onClick={() => {
                       hiddenFileInput.current?.click();
-                    }
-                  }}
-                >
-                  Usar Novamente
-                </Button>
-              );
-            } else if (!videoInfo) {
-              return (
-                <Button
-                  variant="contained"
-                  onClick={() => {
-                    hiddenFileInput.current?.click();
-                  }}
-                >
-                  Anexar Vídeo
-                </Button>
-              );
-            } else {
-              return (
-                <Button
-                  variant="contained"
-                  onClick={async () => {
-                    setVideoWithSubsInfo(null);
+                    }}
+                  >
+                    Anexar Vídeo
+                  </Button>
+                );
+              } else {
+                return (
+                  <Button
+                    variant="contained"
+                    onClick={async () => {
+                      setVideoWithSubsInfo(null);
 
-                    try {
-                      const apiRoute =
-                        process.env.REACT_APP_API_ROUTE ||
-                        "http://127.0.0.1:8000";
-                      const formData = new FormData();
-                      formData.append("request_video", videoInfo.file);
-                      const response = await axios.post(apiRoute, formData, {
-                        responseType: "blob",
-                        headers: {
-                          "Content-Type": "multipart/form-data",
-                        },
-                      });
+                      try {
+                        const apiRoute =
+                          process.env.REACT_APP_API_ROUTE ||
+                          "http://127.0.0.1:8000";
 
-                      setVideoWithSubsInfo({
-                        file: response.data,
-                        src: URL.createObjectURL(response.data),
-                      });
-                    } catch (e) {
-                      console.log(e);
-                      alert("Falha ao tentar aplicar legendas");
-                      setVideoWithSubsInfo(undefined);
-                    }
-                  }}
-                  disabled={videoWithSubsInfo !== undefined}
-                >
-                  Aplicar Legenda
-                </Button>
-              );
-            }
-          })()}
+                        const formData = new FormData();
+                        formData.append("request_video", videoInfo.file);
+                        if (language !== "auto") {
+                          formData.append("language", language);
+                        }
+
+                        const response = await axios.post(apiRoute, formData, {
+                          responseType: "blob",
+                          headers: {
+                            "Content-Type": "multipart/form-data",
+                          },
+                        });
+
+                        setVideoWithSubsInfo({
+                          file: response.data,
+                          src: URL.createObjectURL(response.data),
+                        });
+                      } catch (e: any) {
+                        console.log(e);
+                        try {
+                          const err = JSON.parse(await e.response.data.text());
+                          alert(err.detail);
+                        } catch {
+                          alert("Falha ao tentar aplicar legendas");
+                        }
+                        setVideoWithSubsInfo(undefined);
+                      }
+                    }}
+                    disabled={videoWithSubsInfo !== undefined}
+                  >
+                    Aplicar Legenda
+                  </Button>
+                );
+              }
+            })()}
+
+            <FormControl variant="outlined" className="select">
+              <InputLabel id="language-select-label">Idioma</InputLabel>
+              <Select
+                labelId="language-select-label"
+                value={language}
+                label="Idioma"
+                onChange={(e) => {
+                  setLanguage(e.target.value as Language);
+                }}
+                inputProps={{ readOnly: videoWithSubsInfo !== undefined }}
+              >
+                {[["auto", "Detectar Automaticamente"]]
+                  .concat(Object.entries(languagesDict))
+                  .map(([key, value]) => {
+                    return (
+                      <MenuItem key={key} value={key}>
+                        {value}
+                      </MenuItem>
+                    );
+                  })}
+              </Select>
+            </FormControl>
+          </div>
 
           {!!videoInfo && (
             <div className="video-container">
